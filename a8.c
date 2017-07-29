bf016e247c34b8518b3514dea650e9b169901e60
dedicautus45 <30545733+dedicautus45@users.noreply.github.com>
2017-07-29 15:16:46 +0530

    Add files via upload

---
+++ a8.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<assert.h>
+int sum(int n)
+{int a,b=0;
+while (n>0)
+{
+a=n%10;
+b=b+a;
+n=n/10;
+}
+return b;
+}
+main()
+{
+	int n;
+        printf("Enter number \n");
+	scanf("%d",&n);
+	assert(sum(n)==5);
+        printf("\n The sum is : %d  \n",sum(n));
+}
+
